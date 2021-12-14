7f89e0153622012a6a7eb3f1f75429da923e92c9
crayo <crayo@LAPTOP-QBONQ08M>
2021-12-14 09:47:50 -0700

    gf

--- src/main/webapp/resources/scroller.js
+++ src/main/webapp/resources/scroller.js
@@ -9,7 +9,7 @@ const flightPath = {
 
 for(var i = 0; i <dandy.length; i++) {
 	dandy[i].addEventListener('mouseover', mouseOverEffect);
-	//dandy[i].addEventListener('mouseout', mouseOutEffect);
+	dandy[i].addEventListener('mouseout', mouseOutEffect);
 }
 
 
@@ -19,6 +19,6 @@ function mouseOverEffect() {
 	this.classList.add("seed-visible");
 }
 
-/*function mouseOutEffect() {
+function mouseOutEffect() {
 	this.classList.remove("seed-visible");
-} */
+} 
